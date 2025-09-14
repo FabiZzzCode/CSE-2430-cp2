218c4ee51de74b91469f8a1ef36439bfd52053e4
Fabiya <fabiyaanjum03@gmail.com>
2025-09-14 22:43:36 +0600

    Create Codeforces Round 1050 _ B.cpp

---
+++ Codeforces Round 1050 _ B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n, m;
+        long long x, y;
+        cin >> n >> m >> x >> y;
+
+        for (int i = 0; i < n; ++i) {
+            long long a;
+            cin >> a;
+        }
+        for (int j = 0; j < m; ++j) {
+            long long b;
+            cin >> b;
+        }
+        cout << (n + m) <<endl;
+    }
+    return 0;
+}
